4466858792bd74eb0fc221e9135892829461d4b2
Zaire Ali <zaireali649@gmail.com>
2024-10-21 21:05:17 -0400

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,14 @@
+def get_ec2_client():
+    return "ec2 client"
+
+
+def create_ubuntu_instance(client):
+    return 0
+
+
+def create_amazon_linux_2023_instance(client):
+    return 0
+
+
+def create_amazon_linux_2_instance(client):
+    return 0
